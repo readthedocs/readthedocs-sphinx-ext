ddbec7148f3412bc7a51b6e1f98ddf46e33f54db
Eric Holscher <eric@ericholscher.com>
2013-10-29 08:09:23 -0700

    Bump Version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='readthedocs-sphinx-ext',
-    version='0.3.3',
+    version='0.3.4',
     author='Eric Holscher',
     author_email='eric@ericholscher.com',
     url='http://github.com/ericholscher/readthedocs-sphinx-ext',
