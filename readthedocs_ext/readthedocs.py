b0711e8c77bd7f605cd37c4705c651efde44b4e8
Eric Holscher <eric@ericholscher.com>
2015-06-08 17:03:11 -0700

    Clean up futzing, and make it functional again.

--- readthedocs_ext/readthedocs.py
+++ readthedocs_ext/readthedocs.py
@@ -74,7 +74,8 @@ def finalize_media(builder, local=False):
         if file in MEDIA_MAPPING.keys():
             builder.script_files[index] = MEDIA_MAPPING[file] % MEDIA_URL
             if file == "_static/jquery.js":
-                builder.script_files.insert(index + 1, "%sjavascript/jquery/jquery-migrate-1.2.1.min.js" % MEDIA_URL)
+                builder.script_files.insert(
+                    index + 1, "%sjavascript/jquery/jquery-migrate-1.2.1.min.js" % MEDIA_URL)
 
     if local:
         if 'html_theme' in context and context['html_theme'] == 'sphinx_rtd_theme':
@@ -94,7 +95,8 @@ def finalize_media(builder, local=False):
     # documentation without rebuilding every one.
     # If this script is embedded in each build,
     # then updating the file across all docs is basically impossible.
-    builder.script_files.append('%sjavascript/readthedocs-doc-embed.js' % MEDIA_URL)
+    builder.script_files.append(
+        '%sjavascript/readthedocs-doc-embed.js' % MEDIA_URL)
     builder.css_files.append('%scss/readthedocs-doc-embed.css' % MEDIA_URL)
 
 
@@ -104,7 +106,8 @@ def finalize_comment_media(builder):
     builder.page_hash_mapping = defaultdict(list)
     builder.metadata_mapping = defaultdict(list)
     try:
-        builder.comment_metadata = builder.storage.get_project_metadata(builder.config.html_context['slug'])['results']
+        builder.comment_metadata = builder.storage.get_project_metadata(
+            builder.config.html_context['slug'])['results']
         for obj in builder.comment_metadata:
             builder.metadata_mapping[obj['node']['page']].append(obj['node'])
     except:
@@ -116,7 +119,8 @@ def finalize_comment_media(builder):
     # add our custom bits
     builder.script_files.append('_static/jquery.pageslide.js')
     # builder.script_files.append('_static/websupport2-bundle.js')
-    builder.script_files.append('%sjavascript/websupport2-bundle.js' % MEDIA_URL)
+    builder.script_files.append(
+        '%sjavascript/websupport2-bundle.js' % MEDIA_URL)
     builder.css_files.append('_static/websupport2.css')
     builder.css_files.append('_static/sphinxweb.css')
     builder.css_files.append('_static/jquery.pageslide.css')
@@ -212,156 +216,23 @@ class ReadtheDocsSingleFileHTMLBuilderLocalMedia(SingleFileHTMLBuilder):
         finalize_media(self, local=True)
 
 
-class canonicalnode(nodes.paragraph):
-    pass
-
-
-def add_data_node(app, doctree, docname):
-    new_node = canonicalnode()
-    new_node.rtd_state = app.rtd_state
-    doctree.append(new_node)
-
-
-def visit_canonical(self, node):
-    self.body.append('<!-- Canonical Node for %s --> ' % node.rtd_state.root)
-    pass
-
-
-def depart_canonical(self, node):
-    pass
-
-
-def add_ga_javascript(app, pagename, templatename, context, doctree):
-    meta_output = ""
-    builder = app.config.builder
-    if 'dirhtml' in builder:
-        canonical_page = pagename + "/"
-    else:
-        canonical_page = pagename + ".html"
-
-    if app.rtd_state.canonical_url:
-        if app.rtd_state.single_version:
-            meta_output += """
-            <!--
-            Single version, so link without a language or version
-            http://docs.readthedocs.org/en/latest/canonical.html
-            -->
-            <link rel="canonical" href="{canonical_url}{canonical_page}" />
-            """
-        else:
-            meta_output += """
-            <!--
-            Always link to the latest version, as canonical.
-            http://docs.readthedocs.org/en/latest/canonical.html
-            -->
-            <link rel="canonical" href="{canonical_url}{canonical_language}/latest/{canonical_page}" />
-            """
-
-    else:
-        meta_output += """
-        <!--
-        Read the Docs is acting as the canonical URL for your project.
-        If you want to change it, more info is available in our docs:
-          http://docs.readthedocs.org/en/latest/canonical.html
-        -->
-        <link rel="canonical" href="{canonical_url}{canonical_language}/latest/{canonical_page} }}" />
-        """
-
-    meta_output.format(
-        canonical_url=app.rtd_state.canonical_url,
-        canonical_language=app.rtd_state.language,
-        canonical_page=canonical_page.replace("index.html", "").replace("index/", ""),
-    )
-
-    """
-    {% set canonical_page = pagename + ending %}
-    {% if canonical_url %}
-      {% if single_version %}
-      {% else %}
-      {% endif %}
-    {% else %}
-    <!-- 
-    Read the Docs is acting as the canonical URL for your project. 
-    If you want to change it, more info is available in our docs:
-      http://docs.readthedocs.org/en/latest/canonical.html
-    -->
-    <link rel="canonical" href="http://{{ slug }}.readthedocs.org/{{ rtd_language }}/latest/{{ canonical_page.replace("index.html", "").replace("index/", "") }}" />
-    {% endif %}
-    <script type="text/javascript">
-      // This is included here because other places don't have access to the pagename variable.
-      var READTHEDOCS_DATA = {
-        project: "{{ slug }}",
-        version: "{{ current_version }}",
-        language: "{{ rtd_language }}",
-        page: "{{ pagename }}",
-        builder: "sphinx",
-        theme: "{{ html_theme }}",
-        docroot: "{{ conf_py_path }}",
-        source_suffix: "{{ source_suffix }}",
-        api_host: "{{ api_host }}",
-        commit: "{{ commit }}"
-      }
-      // Old variables
-      var doc_version = "{{ current_version }}";
-      var doc_slug = "{{ slug }}";
-      var page_name = "{{ pagename }}";
-      var html_theme = "{{ html_theme }}";
-    </script>
-    <!-- RTD Analytics Code -->
-    <!-- Included in the header because you don't have a footer block. -->
-    <script type="text/javascript">
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-17997319-1']);
-      _gaq.push(['_trackPageview']);
-    {% if analytics_code %}
-      // User Analytics Code
-      _gaq.push(['user._setAccount', '{{ analytics_code }}']);
-      _gaq.push(['user._trackPageview']);
-      // End User Analytics Code
-    {% endif %}
-
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-    </script>
-
-    """
-
-    metatags = context.get('metatags', '')
-    metatags += """<script type="text/javascript">
-
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', '%s']);
-      _gaq.push(['_trackPageview']);
-
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-    </script>""" % app.config.googleanalytics_id
-    context['metatags'] = metatags
-
-
 def setup(app):
     app.add_builder(ReadtheDocsBuilder)
     app.add_builder(ReadtheDocsDirectoryHTMLBuilder)
     app.add_builder(ReadtheDocsSingleFileHTMLBuilder)
     app.add_builder(ReadtheDocsSingleFileHTMLBuilderLocalMedia)
     app.connect('build-finished', copy_media)
-    app.connect('doctree-resolved', add_data_node)
-    app.add_node(canonicalnode, html=(visit_canonical, depart_canonical))
-    app.connect('html-page-context', add_ga_javascript)
 
     # Comments
     # app.connect('env-updated', add_comments_to_doctree)
-    app.add_directive('comment-configure', directive.CommentConfigurationDirective)
+    app.add_directive(
+        'comment-configure', directive.CommentConfigurationDirective)
     app.add_builder(ReadtheDocsBuilderComments)
     app.add_builder(ReadtheDocsDirectoryHTMLBuilderComments)
-    app.add_config_value('websupport2_base_url', 'http://localhost:8000/websupport', 'html')
-    app.add_config_value('websupport2_static_url', 'http://localhost:8000/static', 'html')
+    app.add_config_value(
+        'websupport2_base_url', 'http://localhost:8000/websupport', 'html')
+    app.add_config_value(
+        'websupport2_static_url', 'http://localhost:8000/static', 'html')
 
     # Embed
     app.add_directive('readthedocs-embed', EmbedDirective)
