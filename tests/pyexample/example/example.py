322329317022a7d1595eb2b23aa97d67fb92779c
Eric Holscher <eric@ericholscher.com>
2016-01-21 16:01:00 -0800

    Test media insertion

--- tests/pyexample/example/example.py
+++
@@ -1,6 +0,0 @@
-import math
-
-
-def example_function(x):
-    """Compute the square root of x and return it."""
-    return math.sqrt(x)
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -7,15 +7,15 @@ from sphinx.application import Sphinx
 
 class LanguageIntegrationTests(unittest.TestCase):
 
-    def _run_test(self, test_dir, test_file, test_string):
+    def _run_test(self, test_dir, test_file, test_string, builder='html'):
         os.chdir('tests/{0}'.format(test_dir))
         try:
             app = Sphinx(
                 srcdir='.',
                 confdir='.',
-                outdir='_build/text',
+                outdir='_build/%s' % builder,
                 doctreedir='_build/.doctrees',
-                buildername='text',
+                buildername='%s' % builder,
             )
             app.build(force_all=True)
             with open(test_file) as fin:
@@ -26,11 +26,18 @@ class LanguageIntegrationTests(unittest.TestCase):
             os.chdir('../..')
 
 
-class PythonTests(LanguageIntegrationTests):
+class IntegrationTests(LanguageIntegrationTests):
 
     def test_integration(self):
         self._run_test(
             'pyexample',
-            '_build/text/index.txt',
-            'Hey there friend!'
+            '_build/html/index.html',
+            'Hey there friend!',
+        )
+
+    def test_media_integration(self):
+        self._run_test(
+            'pyexample',
+            '_build/html/index.html',
+            'media.readthedocs.org',
         )
