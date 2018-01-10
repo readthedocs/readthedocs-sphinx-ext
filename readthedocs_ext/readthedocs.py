a4ab909d9d37a69ec30a728784d8b26b99f83dc6
David Fischer <djfische@gmail.com>
2018-01-10 13:11:25 -0800

    Remove dynamic include file
    - The dynamic include does GA tracking which the plan is to do
      from a script loaded from RTD itself

--- readthedocs_ext/readthedocs.py
+++ readthedocs_ext/readthedocs.py
@@ -43,9 +43,6 @@ def finalize_media(app):
             if file == "_static/jquery.js":
                 app.builder.script_files.insert(
                     index + 1, "%sjavascript/jquery/jquery-migrate-1.2.1.min.js" % MEDIA_URL)
-    app.builder.script_files.append(
-        '%sjavascript/readthedocs-doc-embed.js' % MEDIA_URL
-    )
 
 
 def update_body(app, pagename, templatename, context, doctree):
@@ -88,6 +85,7 @@ def update_body(app, pagename, templatename, context, doctree):
             # Render Read the Docs content
             template_context = render_context.copy()
             template_context['rtd_css_url'] = '%scss/readthedocs-doc-embed.css' % MEDIA_URL
+            template_context['rtd_js_url'] = '%sjavascript/readthedocs-doc-embed.js' % MEDIA_URL
             source = os.path.join(
                 os.path.abspath(os.path.dirname(__file__)),
                 '_templates',
@@ -116,7 +114,6 @@ def update_body(app, pagename, templatename, context, doctree):
 class HtmlBuilderMixin(BuilderMixin):
 
     static_readthedocs_files = [
-        'readthedocs-dynamic-include.js_t',
         'readthedocs-data.js_t',
         # We patch searchtools and copy it with a special handler
         # 'searchtools.js_t'
