185c84654a1559321709166b14c8304060373c0f
David Fischer <djfische@gmail.com>
2018-06-06 11:21:39 -0700

    Bump version for 0.5.11

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='readthedocs-sphinx-ext',
-    version='0.5.10',
+    version='0.5.11',
     author='Eric Holscher',
     author_email='eric@ericholscher.com',
     url='http://github.com/ericholscher/readthedocs-sphinx-ext',
