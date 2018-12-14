addf69becb2a8b34baf3449a86df91df7c4184c5
David Fischer <djfische@gmail.com>
2018-12-14 09:07:42 -0800

    Increment version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='readthedocs-sphinx-ext',
-    version='0.5.15',
+    version='0.5.16',
     author='Read the Docs, Inc',
     author_email='dev@readthedocs.com',
     url='http://github.com/rtfd/readthedocs-sphinx-ext',
