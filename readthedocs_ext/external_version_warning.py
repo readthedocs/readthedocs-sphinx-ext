07bc418d2396b56c0e71c31358e1be453469ae49
saadmk11 <saad.mk11@hotmail.com>
2019-07-10 21:57:04 +0600

    initial external version warning added

---
+++ readthedocs_ext/external_version_warning.py
@@ -0,0 +1,28 @@
+"""
+Add the ability to warning banner for external versions in every page.
+
+If the version type is external this will show a warning banner
+at the top of each page of the documentation.
+"""
+
+from docutils import nodes
+
+try:
+    # Avaliable from Sphinx 1.6
+    from sphinx.util.logging import getLogger
+except ImportError:
+    from logging import getLogger
+
+log = getLogger(__name__)
+
+
+def process_meta(app, doctree):
+    for document in doctree.traverse(nodes.document):
+        text = 'This is an External Version created from pull/merge request.'
+        prose = nodes.paragraph(text, text)
+        warning = nodes.warning(prose, prose)
+        document.insert(0, warning)
+
+
+def setup(app):
+    app.connect('doctree-resolved', process_meta)
