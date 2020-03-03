7421490988d3a26878d10c48a698ed9cb33aa9e6
Michał Górny <mgorny@gentoo.org>
2020-03-03 07:28:29 +0100

    Stop installing tests
    Add a missing exclude to find_packages() in order to stop it from
    installing global-scope 'tests' package.
    
    Fixes #64

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description='Sphinx extension for Read the Docs overrides',
     install_requires=['requests', 'Jinja2>=2.9'],
     package_dir={'': '.'},
-    packages=find_packages('.'),
+    packages=find_packages('.', exclude=['tests']),
     long_description=codecs.open("README.rst", "r", "utf-8").read(),
     # trying to add files...
     include_package_data=True,
