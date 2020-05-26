577a941f48289cc0dd85d9bac3bc807d349102df
Eric Holscher <eric@ericholscher.com>
2020-05-26 09:32:33 -0700

    Release 1.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='readthedocs-sphinx-ext',
-    version='1.0.3',
+    version='1.0.4',
     author='Read the Docs, Inc',
     author_email='dev@readthedocs.com',
     url='http://github.com/readthedocs/readthedocs-sphinx-ext',
