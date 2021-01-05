ccf1f5accd582a164e759665c53b7096e6110db3
Santos Gallegos <stsewd@protonmail.com>
2021-01-05 18:00:22 -0500

    Release 2.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='readthedocs-sphinx-ext',
-    version='2.1.1',
+    version='2.1.2',
     author='Read the Docs, Inc',
     author_email='dev@readthedocs.com',
     url='http://github.com/readthedocs/readthedocs-sphinx-ext',
