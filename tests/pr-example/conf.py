00d704b78b7b6f18de22265713dd37e0104740f5
Santos Gallegos <stsewd@protonmail.com>
2021-01-13 19:03:44 -0500

    PR preview: link to PR and build

---
+++ tests/pr-example/conf.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
+
+extensions = ['readthedocs_ext.external_version_warning']
+
+source_suffix = '.rst'
+master_doc = 'index'
+project = 'pyexample'
+copyright = '2021, readthedocs'
+author = 'readthedocs'
+version = '0.1'
+release = '0.1'
+language = None
+exclude_patterns = ['_build']
+todo_include_todos = False
+html_theme = 'alabaster'
+html_static_path = ['_static']
+htmlhelp_basename = 'pyexampledoc'
+
+html_context = {
+    'slug': 'test-slug',
+    'commit': 'deadd00d',
+    'current_version': '123',
+}
+
+readthedocs_vcs_url = 'https://github.com/readthedocs/readthedocs.org/pull/7826'
+readthedocs_build_url = 'https://readthedocs.org/projects/docs/builds/12759474/'
